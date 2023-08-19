e1a6915170acfe463fa923a1b5fc58da1cc4c4c4
Harry MacCode <emarh.harry.code@gmail.com>
2023-08-19 11:25:48 +0200

    Register User : Validation

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,5 +1,31 @@
-const registerUser = async (req, res) => {
-    res.send('User Registration');
-};
+const asyncHandler = require('express-async-handler');
+const User = require('../models/user.model');
+const bcrypt = require('bcryptjs');
+
+const registerUser = asyncHandler(async (req, res) => {
+    const { name, email, password } = req.body;
+
+    // Validation
+    if (!name || !email || !password) {
+        res.status(400)
+        throw new Error('Veuillez renseigner tous les champs obligatoires');
+    }
+
+    if (password.length < 6) {
+        res.status(400)
+        throw new Error('Le mot de passe doit avoir au moins 6 caractères');
+    }
+
+    // Check if user exists
+    const userExists = await User.findOne({ email });
+
+    if (userExists) {
+        res.status(400)
+        throw new Error('Cet email exist déjà !');
+    }
+
+    // Create new User
+    const user = await User.create({ name, email, password });
+});
 
 module.exports.registerUser = registerUser;
