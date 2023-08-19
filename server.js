884a92062551a365d5ac718a57b4a8ce598759ee
Harry MacCode <emarh.harry.code@gmail.com>
2023-08-19 09:09:35 +0200

    Setup of middlewares: express.json, express.urlencoded, cookieParser, bodyParser.json

--- server.js
+++ server.js
@@ -6,13 +6,26 @@ const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 
 const app = express();
-
 const PORT = process.env.PORT || 5010;
 
-app.get('/', (rea, res) => {
+// Middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(
+    cors({
+        origin: ['http://localhost:3000', 'https://authz-app.vercel.app'],
+        credentials: true
+    })
+)
+
+app.get('/', (req, res) => {
     res.send('Home Page');
 });
 
+
+app.listen(PORT, console.log(`Server is running on the port ${PORT}`));
 mongoose
     .connect(process.env.MONGO_URI)
     .then(() => {
