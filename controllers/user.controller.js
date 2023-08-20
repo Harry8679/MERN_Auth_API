be40c9d33953cf2ed7ad0dd318333072db9a6e3c
Harry MacCode <emarh.harry.code@gmail.com>
2023-08-20 10:57:44 +0200

    User Register: Get User Agent

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,6 +1,7 @@
 const asyncHandler = require('express-async-handler');
 const User = require('../models/user.model');
 const { generateToken } = require('../utils/index.util');
+const parser = require('ua-parser-js');
 
 const registerUser = asyncHandler(async (req, res) => {
     const { name, email, password } = req.body;
@@ -24,8 +25,12 @@ const registerUser = asyncHandler(async (req, res) => {
         throw new Error('Cet email exist déjà !');
     }
 
+    // Get UserAgent
+    const ua = parser(req.headers['user-agent']);
+    const userAgent = [ua.ua];
+
     // Create new User
-    const user = await User.create({ name, email, password });
+    const user = await User.create({ name, email, password, userAgent });
 
     // Generate Token
     const token = generateToken(user._id);
